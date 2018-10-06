ff810a47875eab3d62892cd85a29671ff4b36b3c
abejide001 <abejidefemi1@gmail.com>
2018-10-06 01:45:35 +0100

    commit

--- test/test_calculator_multiplication.js
+++ test/test_calculator_multiplication.js
@@ -1,9 +1,11 @@
 "use strict";
-
+const { multiply } = require('../index')
 const { assert } = require("chai");
 
 describe("multiply()", () => {
-  it("should multiply positive numbers together");
+  it("should multiply positive numbers together", () => {
+    assert.equal(multiply(1,4), 4)
+  } );
 
   it("should multiply positive and negative numbers together");
 
